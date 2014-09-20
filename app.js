a0584844ad8f7fb01c70ceb5ee18e4f429feff26
Jasmina Kruhak <jkruhak@gmail.com>
2014-09-20 09:34:30 +0400

    showCurrentWeather

--- app.js
+++ app.js
@@ -8,26 +8,32 @@ var getCurrentWeather = function(cityName) {
 		type: "GET",
 		dataType: "json",
 		success: function(weatherResult) {
-			//get current weather icon
-			$(".templates .currentWeather").find(".icon").html("<img src='http://api.openweathermap.org/data/2.5/img/w/"+weatherResult.weather[0].icon+"'>");
-			//get city name and country
-			$(".templates .currentWeather").find(".name").html(weatherResult.name + ", " + weatherResult.sys.country);
-			//get current temperature
-			$(".templates .currentWeather").find(".temp").html(weatherResult.main.temp+"&#176; C");
-			//get cloud
-			$(".templates .currentWeather").find(".sky").html(weatherResult.clouds.all);
-			//get current weather description
-			$(".templates .currentWeather").find(".description").html(weatherResult.weather[0].main);
+			showCurrentWeather(weatherResult);
 		},
 		error: function(xhr, status, errorThrown) {
 			console.log("Error: " + errorThrown);
 	        console.log("Status: " + status);
 	        console.dir(xhr);
 		},
-		complete: function( xhr, status ) {
+		complete: function(xhr, status) {
         	console.log("Request has been completed");
     	}
 	});		
+};
+
+var showCurrentWeather = function(weatherResult) {
+	var template = $(".templates .currentWeather");
+
+	//get current weather icon
+	template.find(".icon").html("<img src='http://api.openweathermap.org/data/2.5/img/w/"+weatherResult.weather[0].icon+"'>");
+	//get city name and country
+	template.find(".name").html(weatherResult.name + ", " + weatherResult.sys.country);
+	//get current temperature
+	template.find(".temp").html(weatherResult.main.temp+"&#176; C");
+	//get cloud
+	template.find(".sky").html(weatherResult.clouds.all);
+	//get current weather description
+	template.find(".description").html(weatherResult.weather[0].main);
 };
 
 /*--- jQuery ---*/
@@ -35,6 +41,7 @@ $(document).ready(function() {
 
 	$(".weather").submit(function(event) {
 		var cityName = $(this).find("input[name='tags']").val();
+
 		
 		getCurrentWeather(cityName);
 
